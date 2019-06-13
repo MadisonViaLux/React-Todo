d0c18efc15f70d461636cef4bc99dcaf7aa38850
MadisonViaLux <madison.s.mckown@gmail.com>
2019-06-13 15:23:59 -0700

    it begins....

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import TodoForm from './components/TodoComponents/TodoForm'
 import TodoList from './components/TodoComponents/TodoList'
+import './components/TodoComponents/Todo.scss';
+
 
 class App extends React.Component {
   // you will need a place to store your state in this component.
